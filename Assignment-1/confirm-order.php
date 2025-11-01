3d2e8d7d53816f2de36957b375d64c6e396041d8
Shy-Anna Lee <shyjah89@gmail.com>
2025-11-01 15:46:08 -0400

    outline for confirm page (lesson 4)

--- Assignment-1/confirm-order.php
+++ Assignment-1/confirm-order.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset=""UTF-8">
+    <title> Order Confirmation for HTMLs Pizza Planet</title>
+    <link rel="stylesheet" href="css/styles.css" />
+ </head>
+  
+  <body>
+    <header>
+      <h1>HTMLs Pizza Planet</h1>
+    </header>
 
+    <h2>Your Order has been received!</h2>
+
+    <?php
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+	$fname = $_GET["fname"];
+
+
+	echo '<p>Thank you, ' .$fname .' '!</p>';
+	foreach ($alert as $item) {
+   echo ('<li>'. $item.'</li>
+    }
+	echo('</ul>');
+
+    
+ </body>
+
+  
