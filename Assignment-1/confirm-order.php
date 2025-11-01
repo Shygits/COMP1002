ab17684efb744efa84d4c3c26c1eaacacf6d5ba3
Shy-Anna Lee <shyjah89@gmail.com>
2025-11-01 15:48:51 -0400

    Refactor order confirmation to use new parameters

--- Assignment-1/confirm-order.php
+++ Assignment-1/confirm-order.php
@@ -14,14 +14,14 @@
     <h2>Your Order has been received!</h2>
 
     <?php
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
-	$fname = $_GET["fname"];
+	$fname = $_GET["name"];
+	$size = $_GET["size"];
+	$shape = $_GET["shape"];
+	$crust = $_GET["crust"];
+	$sauce = $_GET["sauce"];
+	$toppings = $_GET["toppings"];
+	$quantity = $_GET["quantity"];
+	$instructions = $_GET["instructions"];
 
 
 	echo '<p>Thank you, ' .$fname .' '!</p>';
